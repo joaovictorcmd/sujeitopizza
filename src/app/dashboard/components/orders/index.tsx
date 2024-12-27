0c22690ae245a206e4a1f43fe849a54b1090e724
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-12-27 14:01:01 -0300

    :rocket: deploy: Deploy application to production environment

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -6,6 +6,8 @@ import { RefreshCw } from "lucide-react"
 import { ModalOrder } from "../modal"
 import { useContext } from "react"
 import { OrderContext } from "@/providers/order"
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
 
 interface Props {
     orders: OrderProps[]
@@ -14,9 +16,15 @@ interface Props {
 export function Orders({ orders }: Props) {
 
     const { isOpen, onRequestOpen } = useContext(OrderContext);
+    const router = useRouter();
 
     async function handleDetailOrder(order_id: string) {
         await onRequestOpen(order_id);
+    }
+
+    function handleRefresh() {
+        router.refresh();
+        toast.success("Pedidos atualizados");
     }
 
     return (
@@ -25,12 +33,21 @@ export function Orders({ orders }: Props) {
 
                 <section className={styles.containerHeader}>
                     <h1>Últimos pedidos</h1>
-                    <button>
+                    <button onClick={handleRefresh}>
                         <RefreshCw size={24} color="#3fffa3" />
                     </button>
                 </section>
 
                 <section className={styles.listOrders}>
+
+                    {
+
+                        orders.length === 0 && (
+                            <span className={styles.emptyItem}>
+                                Nenhum pedido aberto no momento
+                            </span>
+                        )
+                    }
 
                     {orders.map(order => (
                         <button
