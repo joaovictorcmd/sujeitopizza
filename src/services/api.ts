6dea781df5217f3a0d165b1e5e8566b432362508
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2025-01-10 02:16:41 -0300

    test

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API?.startsWith("http")
-    ? process.env.NEXT_PUBLIC_API
-    : `https://${process.env.NEXT_PUBLIC_API}`,
+  baseURL: process.env.NEXT_PUBLIC_API,
 });
 
 // Log para verificar a URL da API
